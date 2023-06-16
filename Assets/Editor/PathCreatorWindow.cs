a37144c36dc058d0bb300734be2b0ee395a7211b
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-06-16 16:13:18 +0800

    (UnassignedRefException) add enemy path generation window

---
+++ Assets/Editor/PathCreatorWindow.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+[SuppressMessage("ReSharper", "CheckNamespace")]
+public class PathCreatorWindow : EditorWindow
+{
+    private float speed;
+    private Vector3 endPosOffset;
+
+    private GameObject patroller;
+    private GameObject traversalObject;
+
+    private EnemyController enemy;
+    
+    [MenuItem("Window/Enemies")]
+    public static void ShowWindow()
+    {
+        GetWindow<PathCreatorWindow>("Create Enemy Patrol Path");
+    }
+    
+    private void OnGUI()
+    {
+        GUILayout.Label("This lets you create enemy patrols");
+        
+        endPosOffset = EditorGUILayout.Vector3Field("End Position", endPosOffset);
+        speed = EditorGUILayout.FloatField("Enemy Speed", speed);
+
+        traversalObject = 
+#pragma warning disable CS0618
+            (GameObject)EditorGUILayout.ObjectField("Point Prefab", traversalObject, typeof(GameObject));
+
+        
+        patroller = 
+            (GameObject)EditorGUILayout.ObjectField("Enemy Position", patroller, typeof(GameObject));
+#pragma warning restore CS0618
+        
+        // ReSharper disable once InvertIf
+        if (GUILayout.Button("Create Path"))
+        {
+            // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+            enemy = patroller.GetComponent<EnemyController>();
+
+            var originalPos = patroller.transform.position;
+            enemy.Speed = speed;
+            enemy.Origin = Instantiate(traversalObject, originalPos , Quaternion.identity);
+            enemy.FirstPoint = Instantiate(traversalObject, originalPos + endPosOffset, Quaternion.identity);  
+            
+            Debug.Log("done");
+        }
+    }
+}
