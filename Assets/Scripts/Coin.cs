f4c317d5da1b3973a5908eede10e1327960fe547
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-06-17 23:19:38 +0800

    overall cleanup in coin.cs

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,25 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
     [SerializeField] private ScoreManager score;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
