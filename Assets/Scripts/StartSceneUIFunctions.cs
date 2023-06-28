c39d69354fc382d14ef0a98471e416d182d2f6a0
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-06-28 17:58:17 +0800

    add UI buttons and start screen

---
+++ Assets/Scripts/StartSceneUIFunctions.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneUIFunctions : MonoBehaviour
+{
+    public void StartButton()
+    {
+        print("start");
+    }
+
+    public void ExitButton()
+    {
+        print("exit");
+    }
+}
