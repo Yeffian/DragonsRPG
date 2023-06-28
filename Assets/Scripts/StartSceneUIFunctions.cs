5d85ca5a0c913b55914694abdd631f180b99961c
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-06-28 18:35:57 +0800

    added exit button in editor and build

--- Assets/Scripts/StartSceneUIFunctions.cs
+++ Assets/Scripts/StartSceneUIFunctions.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartSceneUIFunctions : MonoBehaviour
 {
     public void StartButton()
     {
-        print("start");
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 
     public void ExitButton()
     {
-        print("exit");
+#if UNITY_EDITOR
+        if(EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+#endif
+
+        Application.Quit();
     }
 }
