17d9d51f9ce42889d08109f35257ae5ccea7374d
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-07-13 17:38:08 +0800

    add base class for moving objects

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,36 +2,18 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class EnemyController : MonoBehaviour
+public class EnemyController : MovingObject
 {
-    private enum TraversalDirection
-    {
-        OriginToFirst,
-        FirstToOrigin
-    }
-
     [SerializeField] public GameObject FirstPoint;
     [SerializeField] public GameObject Origin;
     [SerializeField] public float Speed;
-
-    private TraversalDirection _dir;
-    private GameObject _target;
-
-    private TraversalDirection ReverseDirection(TraversalDirection dir)
-        => dir == TraversalDirection.OriginToFirst ? TraversalDirection.FirstToOrigin : TraversalDirection.OriginToFirst;
-
-         // Start is called before the first frame update
-    void Start()
-    {
-        _target = FirstPoint;
-        _dir = TraversalDirection.OriginToFirst;
-    }
 
     // Update is called once per frame
     void Update()
     {
         // Thank you nt314p for this clever solution
-        transform.position = Vector3.Lerp(FirstPoint.transform.position, Origin.transform.position, Mathf.PingPong(Time.time * Speed, 1));
+        float t = Mathf.PingPong(Time.time * Speed, 1);
+        transform.position = Vector3.Lerp(FirstPoint.transform.position, Origin.transform.position, t);
     }
 
     private void OnCollisionEnter2D(Collision2D col)
