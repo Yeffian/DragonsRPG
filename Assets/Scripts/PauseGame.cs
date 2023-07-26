0f7030090c3695be8860ed197ea457e409c61b14
YeffyCodeGit <aditchakraborty2009@gmail.com>
2023-07-26 09:29:49 +0600

    refactor code to enable pause menu

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -21,16 +21,15 @@ public class PauseGame : MonoBehaviour
             if (_paused == false)
             {
                 Time.timeScale = 0.0f;
-                pauseScreen.SetActive(true);
                 _paused = !_paused;
             }
             else if (_paused)
             {
                 Time.timeScale = _timeScale;
-                pauseScreen.SetActive(false);
                 _paused = !_paused;
             }
         }
         
+        pauseScreen.SetActive(_paused);
     }
 }
